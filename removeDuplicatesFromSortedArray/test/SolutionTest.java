bbfc3543daee0e5e760ac17ed66bd4b22a541209
falcon.nick@gmail.com <kitty793>
2017-09-22 12:40:57 -0500

    added setup method to cleanup unit tests.

--- removeDuplicatesFromSortedArray/test/SolutionTest.java
+++ removeDuplicatesFromSortedArray/test/SolutionTest.java
@@ -1,33 +1,42 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SolutionTest {
+
+    private Solution s;
+
+    @BeforeEach
+    void setUp() {
+        s = new Solution();
+    }
+
     @Test
     void sampleInput() {
         int[] input = new int[] {1,1,2};
-        Solution s = new Solution();
+
         assertEquals(2, s.removeDuplicates(input));
     }
 
     @Test
     void EmptyInput() {
         int[] input = new int[] {};
-        Solution s = new Solution();
+
         assertEquals(0, s.removeDuplicates(input));
     }
 
     @Test
     void LongerInput() {
         int[] input = {1,1,2,3,4,4,5};
-        Solution s = new Solution();
+
         assertEquals(5, s.removeDuplicates(input));
     }
 
     @Test
     void RepeatedNumber() {
         int[] input = {1,1,1};
-        Solution s = new Solution();
+
         assertEquals(1, s.removeDuplicates(input));
     }
 }
